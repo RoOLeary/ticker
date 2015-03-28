1c968f88ac225373f2d140e1fb70167b2e8e5e5a
Ro_OLeary <roleary81@gmail.com>
2015-03-28 14:02:53 +0000

    Commit ticker js with defaults

--- js/ticker.js
+++ js/ticker.js
@@ -1,22 +1,29 @@
 jQuery(document).ready(function(){
-
-	if(ticker_vars.date == ''){
-		ticker_vars.date == '2015-12-31';
-	}
 	console.log( ticker_vars.date + ' ' + ticker_vars.time);
+
+		if ((ticker_vars.date) == ''){
+			ticker_vars.date = '2015-12-31';
+			console.log('null' + ticker_vars.date);
+		};
+
+		if ((ticker_vars.time) == ''){
+			ticker_vars.time = '23:59';
+			console.log('null' + ticker_vars.date);
+		};
 	
-	var targetElements = '.ticker-plugin';
-	var now = new Date();
-	var nowISO = now.toISOString();
-	var target0 = new Date(ticker_vars.date + ' ' + ticker_vars.time + ' ' + "UTC");
-	var targetISO0 = target0.toISOString();
+		var targetElements = '.ticker';
+		var now = new Date();
+		var nowISO = now.toISOString();
+		var target0 = new Date(ticker_vars.date + ' ' + ticker_vars.time + ' ' + "UTC");	
+		var targetISO0 = target0.toISOString();
 
-	if ( nowISO < targetISO0 ) {
 
-		jQuery(targetElements).countdown({until: target0, format: "dHMS", compact: true, onExpiry: textUpdate});
+		if ( nowISO < targetISO0 ) {
 
-	} 
+			jQuery(targetElements).countdown({until: target0, format: "dHMS", compact: true, onExpiry: textUpdate});
 
+		} 
+		
 	function textUpdate(){
 		return 'Expired';
 
